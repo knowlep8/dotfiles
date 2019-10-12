2e51a8f2a5a0ccecd92e2959cd4f123bdd14eafd
Paul Knowles <ptk@apple.com>
2019-10-11 20:03:57 -0400

    Added tmux theme from plugin, fixed setup.sh and README

--- setup.sh
+++ setup.sh
@@ -6,5 +6,4 @@ echo "export EDITOR=vim" >> ~/.profile
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/.liquidpromptrc ~/.liquidpromptrc
 ln -s ~/dotfiles/.tmux ~/.tmux
